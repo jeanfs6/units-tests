dc99603e8589a94c5517008fd630e1aafe87c961
jean <jeansantos@id.uff.br>
2022-03-08 20:27:59 -0300

    adicionando de forma correta o item 2

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -16,15 +16,21 @@ const numbers = require('../src/numbers');
 */
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers`retorna `true` quando o array contém apenas numeros e falso caso contrário', () => {
-
-    // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+   // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+   it('Verifica se a função recebe um array de números e retorna true', () => {
     expect(numbers([1, 2, 3, 4, 5])).toBeTruthy();
+  });
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+    it('Verifica se a função recebe um array e retorna false', () => {
     expect(numbers([1, 2, '3', 4, 5])).toBeFalsy()
+  });
+  
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+    it('Verifica se a função recebe um array de númers contendo string e retorna false', () => {
     expect(numbers([1,'a',3])).toBeFalsy()
+  });
     // Escreva um teste em que a função recebe [' '] e retorna false
+    it('Verifica se a função recebe um array vazio e retorna false', () => {
     expect(numbers([''])).toBeFalsy()
   });
 });
